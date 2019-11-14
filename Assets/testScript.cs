95f1dc781af1e257d9a015c3776a50433448be97
risingtsun <patrickdevota@gmail.com>
2019-11-13 23:42:40 -0800

    imported the things i worked on in lab today
    NEEDED:
    -fix the rumble1 playing twice
    -maybe get new rumble sounds that are more pleasing?
    -encapsulate testScript functionality into the AudioManager2
    -have a function that correlates the train speed from Donny's train and converts it to the playback animation speed of sound.anim

---
+++ Assets/testScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public AudioClip SoundToPlay1;
+    public AudioClip SoundToPlay2;
+    public AudioSource source1;
+    public AudioSource source2;
+    public Animator anim;
+
+    void Awake()
+    {
+        //source = GetComponent<AudioSource>();
+        AudioSource[] audioSources = Object.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        anim = gameObject.GetComponent<Animator>();
+        source1 = audioSources[0];
+        source2 = audioSources[1];
+    }
+
+    /*
+    void playSound()
+    {
+        Debug.Log("Playing sound: " + SoundToPlay);
+        source.PlayOneShot(SoundToPlay1);
+    }
+     * */
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //playSound();
+            anim.Play("sound");
+            //anim.Play("sound 1");
+        }
+    }
+
+    void playSound1()
+    {
+        Debug.Log("Playing sound: " + SoundToPlay1);
+        //later connect AudioManager2 to use the play sound function that can adjust the pitch
+        source1.PlayOneShot(SoundToPlay1);
+    }
+
+    void playSound2()
+    {
+        Debug.Log("Playing sound: " + SoundToPlay2);
+        //later connect AudioManager2 to use the play sound function that can adjust the pitch
+        source2.PlayOneShot(SoundToPlay2);
+    }
+
+    void foo()
+    {
+		Debug.Log("foo");
+    }
+}
