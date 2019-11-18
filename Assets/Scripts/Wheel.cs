7e0578278cb1928afe8205038b0f416d86248ae1
EnderPicture <enderpicture@gmail.com>
2019-11-17 21:00:37 -0800

    added new player sprites

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    public Transform locator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float circumference  = 2*Mathf.PI*(locator.position-transform.position).magnitude;
+        transform.localRotation = Quaternion.Euler(0,0,-(transform.position.x/circumference)*360);
+    }
+}
