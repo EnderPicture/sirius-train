36cf446ac7da7a58f2312ce8312afe48433e24ab
EnderPicture <enderpicture@gmail.com>
2019-12-04 00:00:30 -0800

    star system working on it

---
+++ Assets/StarSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StarSystem : MonoBehaviour
+{
+    public SpriteRenderer star1;
+    public SpriteRenderer star2;
+    public SpriteRenderer star3;
+    public SpriteRenderer star4;
+    public SpriteRenderer star5;
+
+    public void setScore(int score)
+    {
+        DOTween.Init();
+        score = Mathf.Clamp(score, 0, 5);
+        if (score > 0)
+        {
+            star1.DOFade(0, .25f);
+        }
+        if (score > 1)
+        {
+            star2.DOFade(0, .25f);
+        }
+        if (score > 2)
+        {
+            star3.DOFade(0, .25f);
+        }
+        if (score > 3)
+        {
+            star4.DOFade(0, .25f);
+        }
+        if (score > 4)
+        {
+            star5.DOFade(0, .25f);
+        }
+    }
+}
